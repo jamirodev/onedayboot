bec339af62c42ca74c16626f5a0c71e39e96d322
jamirodev <jamirodev@gmail.com>
2023-08-01 17:32:38 +0900

    154

--- src/main/java/com/yse/dev/book/entity/Book.java
+++ src/main/java/com/yse/dev/book/entity/Book.java
@@ -10,12 +10,16 @@ import javax.persistence.Id;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
